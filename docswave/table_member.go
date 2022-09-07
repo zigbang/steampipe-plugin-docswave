16c789b7a78e144b730df7a5cfcad45990f7802c
Sunil Jo <joseongil@zigbang.com>
2022-09-07 18:56:44 +0900

    fix(DocswaveMember) Fix members teamId retreiving way

--- docswave/table_member.go
+++ docswave/table_member.go
@@ -40,7 +40,8 @@ func tableDocswaveMember(ctx context.Context) *plugin.Table {
 			{Name: "member_status", Type: proto.ColumnType_STRING},
 			{Name: "member_entry_date", Type: proto.ColumnType_STRING},
 			{Name: "member_role", Type: proto.ColumnType_STRING},
-			{Name: "team_id", Type: proto.ColumnType_STRING, Hydrate: getMemberTeam},
+			{Name: "member_team_list", Type: proto.ColumnType_JSON},
+			{Name: "team_id", Type: proto.ColumnType_STRING},
 			{Name: "created_date", Type: proto.ColumnType_STRING},
 			{Name: "update_date", Type: proto.ColumnType_STRING},
 		},
@@ -62,6 +63,7 @@ type Member struct {
 	MemberStatus      string `json:"memberStatus"`
 	MemberEntryDate   string `json:"memberEntryDate"`
 	MemberRole        string `json:"memberRole"`
+	MemberTeamList    []Team `json:"memberTeamList"`
 	TeamId            string `json:"teamId"`
 	CreatedDate       string `json:"createdDate"`
 	UpdateDate        string `json:"updateDate"`
@@ -71,7 +73,7 @@ type Team struct {
 	TeamId          string `json:"teamId"`
 	TeamName        string `json:"teamName"`
 	ParentTeamId    string `json:"parentTeamId"`
-	ChildTeamModels string `json:"childTeamModels"`
+	ChildTeamModels []Team `json:"childTeamModels"`
 	CreatedDate     string `json:"createdDate"`
 	UpdateDate      string `json:"updateDate"`
 }
@@ -104,7 +106,17 @@ func listMember(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData)
 
 	json.Unmarshal([]byte(htmlData), &members)
 
+	teamsDepth := make(map[string]int)
+	teams, err := getDocswaveTeams(ctx, d)
+	if err != nil {
+		logger.Error("Error-Docswave-getMemberTeam-getDocswaveTeams:", err)
+	}
+	getDocswaveTeamsDepth(teamsDepth, 0, teams)
+
 	for _, member := range members {
+		if member.MemberStatus == "WORKING" {
+			member.TeamId = getDocswaveHighestTeamId(member.MemberTeamList, teamsDepth)
+		}
 		d.StreamListItem(ctx, member)
 	}
 
@@ -144,48 +156,73 @@ func getMember(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) 
 
 	json.Unmarshal([]byte(htmlData), &member)
 
+	teamsDepth := make(map[string]int)
+	teams, err := getDocswaveTeams(ctx, d)
+	if err != nil {
+		logger.Error("Error-Docswave-getMemberTeam-getDocswaveTeams:", err)
+	}
+
+	getDocswaveTeamsDepth(teamsDepth, 0, teams)
+	if member.MemberStatus == "WORKING" {
+		member.TeamId = getDocswaveHighestTeamId(member.MemberTeamList, teamsDepth)
+	}
+
 	return member, nil
 }
 
-func getMemberTeam(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	logger := plugin.Logger(ctx)
-	logger.Trace("Warn-Docswave-getMemberTeam-Start:")
-	hydrateData := h.HydrateResults
-	logger.Trace("getMemberTeam", "hydrateData", hydrateData)
-	var memberId string
-	if hydrateData["listMember"] != nil {
-		if hydrateData["listMember"].(Member).MemberStatus == "WORKING" {
-			memberId = hydrateData["listMember"].(Member).MemberId
-		} else {
-			return nil, nil
+func MinMax(array []int) (int, int) {
+	var max int = array[0]
+	var min int = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
 		}
-	} else {
-		if hydrateData["getMember"].(Member).MemberStatus == "WORKING" {
-			memberId = hydrateData["getMember"].(Member).MemberId
-		} else {
-			return nil, nil
+		if min > value {
+			min = value
 		}
 	}
-	logger.Trace("getMember-memberId", "value", memberId)
+	return min, max
+}
 
+func getDocswaveHighestTeamId(teams []Team, teamsDepth map[string]int) string {
+	var depths []int
+	if len(teams) < 1 {
+		return ""
+	}
+	for _, x := range teams {
+		depths = append(depths, teamsDepth[x.TeamId])
+	}
+
+	highestDepth, _ := MinMax(depths)
+
+	for i, x := range depths {
+		if x == highestDepth {
+			return teams[i].TeamId
+		}
+	}
+	return ""
+}
+
+func getDocswaveTeams(ctx context.Context, d *plugin.QueryData) ([]Team, error) {
+	logger := plugin.Logger(ctx)
 	client, token, err := connect(ctx, d)
 	if err != nil {
-		logger.Error("Error-Docswave-getMemberTeam-connect:", err)
+		logger.Error("Error-Docswave-getDocswaveTeams-connect:", err)
 		return nil, nil
 	}
 
-	url := "https://openapi.docswave.com/members/" + memberId + "/teams?openApiKey=" + token
+	url := "https://openapi.docswave.com/teams/tree?openApiKey=" + token
 
 	resp, err := client.Get(url)
 	if err != nil {
-		logger.Error("Error-Docswave-getMemberTeam-client.Get:", err)
+		logger.Error("Error-Docswave-getDocswaveTeams-client.Get:", err)
 		return nil, nil
 	}
 	defer resp.Body.Close()
 
 	htmlData, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		logger.Error("Error-Docswave-getMemberTeam-ioutil.ReadAll:", err)
+		logger.Error("Error-Docswave-getDocswaveTeams-ioutil.ReadAll:", err)
 		return nil, nil
 	}
 
@@ -193,11 +230,18 @@ func getMemberTeam(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateDa
 
 	json.Unmarshal([]byte(htmlData), &teams)
 
-	logger.Trace("Warn-Docswave-getMemberTeam-Unmarshal:", htmlData, teams)
+	logger.Trace("Warn-Docswave-getDocswaveTeams-Unmarshal:", teams)
 
-	if len(teams) == 0 {
-		return nil, nil
-	} else {
-		return teams[0], nil
+	return teams, nil
+}
+
+func getDocswaveTeamsDepth(teamsDepth map[string]int, depth int, arr []Team) {
+	depth++
+
+	for _, x := range arr {
+		teamsDepth[x.TeamId] = depth
+		if len(x.ChildTeamModels) > 0 {
+			getDocswaveTeamsDepth(teamsDepth, depth, x.ChildTeamModels)
+		}
 	}
 }
